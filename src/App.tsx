366a4671643e7af857842d71b60c4886c7beb7df
Hyunsu Lim <hyunsu.lim01@gmail.com>
2021-08-26 17:38:49 +0900

    Change coding style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const App: React.FC = () => (
-  <div>Hello, World!</div>
-);
+function App() {
+  return (
+    <div>Hello, World!</div>
+  );
+}
 
 export default App;
