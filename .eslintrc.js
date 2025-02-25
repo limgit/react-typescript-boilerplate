f47ad1abf23669e1f23ce15cc631b7be8c9a5d34
Hyunsu Lim <hyunsu.lim01@gmail.com>
2025-02-25 14:14:28 +0900

    Ignore all the rules as styling rules are managed by prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,32 +19,5 @@ module.exports = {
     },
   },
   ignorePatterns: [".eslintrc.js", "webpack.config.js"],
-  rules: {
-    "arrow-body-style": "off",
-    "import/prefer-default-export": "off",
-    "max-len": [
-      "error",
-      {
-        code: 120,
-        tabWidth: 2,
-        ignoreComments: true,
-      },
-    ],
-    "react/prop-types": "off",
-    "react/require-default-props": "off",
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/member-delimiter-style": [
-      "error",
-      {
-        multiline: {
-          delimiter: "comma",
-          requireLast: true,
-        },
-        singleline: {
-          delimiter: "comma",
-          requireLast: true,
-        },
-      },
-    ],
-  },
+  rules: {},
 };
