e27c3dc365158322d8a7b94c8b4a88e82f86e57d
Hyunsu Lim <hyunsu.lim01@gmail.com>
2025-10-03 20:20:55 +0900

    Let info log be shown on non-build commands

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  logLevel: "warn",
   plugins: [react(), tsconfigPaths()],
 });
